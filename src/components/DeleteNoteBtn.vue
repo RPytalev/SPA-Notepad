22f805ad252856dc878e21dd113b2f0652a6daa3
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:15:40 +0300

    Created DeleteNoteBtn component

---
+++ src/components/DeleteNoteBtn.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="notesListHeader">
+        <div class="btn" @click="deleteNote">deleteNote</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DeleteNoteBtn',
+        methods: {
+            deleteNote() {
+                this.$emit('delete-note');
+            }
+        },
+        props: {
+            deleteNoteId: Number
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+.notesListHeader 
+    display: flex
+    justify-content: flex-end
+    align-items: center
+    width: 100%
+    height: 4rem
+    background-color: orange
+
+    .btn
+        width: 10rem
+        height: 2rem
+        background-color: red
+        color: #fff
+</style>
