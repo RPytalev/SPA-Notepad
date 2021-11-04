a860ca9e8ea4a1550533092bc8e045315c99118a
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:15:09 +0300

    Created NewTextareaBtn component

---
+++ src/components/CreateNewTextareaBtn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="notes-editor-header">
+        <button @click="$emit('create-new-textarea')">New Note</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CreateNewTextareaBtn'
+    }
+</script>
+
+<style lang="sass" scoped>
+.notes-editor-header 
+    display: flex
+    justify-content: flex-start
+    align-items: center
+    width: 100%
+    height: 4rem
+    background-color: orange
+
+    button
+        width: 10rem
+        height: 2rem
+        background-color: green
+</style>
