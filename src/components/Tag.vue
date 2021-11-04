3cc61295762cdcad61831d70d4d5cc7c87b5b9c0
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:18:01 +0300

    Created Tag component

---
+++ src/components/Tag.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="tag">
+        {{item}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tag',
+        props: {
+            item: String
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+.tag
+    display: flex
+    width: auto
+    height: 1.5rem
+    padding: .1rem 1rem
+    margin: .2rem .1rem
+    background-color: #000
+    color: red
+    border: .1rem solid #fff
+</style>
