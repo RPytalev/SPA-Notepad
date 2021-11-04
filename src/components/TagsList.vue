cd8b1b2fa0d96546e983f9e9d87d023a968daead
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:17:46 +0300

    Created TagsList component

---
+++ src/components/TagsList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="tags-list">
+        <div class="tags" :key="item.id" v-for="item in tagsList">
+            <Tag :tagsList="tagsList" :item="item" />
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+import Tag from './Tag.vue'
+
+    export default {
+        name: 'TagsList',
+        components: {
+            Tag
+        },
+        props: {
+            tagsList: Array
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+::-webkit-scrollbar
+  width: 0
+
+.tags-list
+    display: flex
+    flex-flow: row nowrap
+    width: 100%
+    height: 100%
+    overflow: auto
+
+    .tags
+        display: flex
+        flex-flow: row wrap
+        width: 100%
+        height: 100%
+        background-color: #ff5800
+</style>
