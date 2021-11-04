10a3609f7e7ff7d1630219019122e61a7e2e49a8
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:12:11 +0300

    Created NotesList component

---
+++ src/components/NotesList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="notes-list">
+        <div :key="item.id" v-for="item in notesList">
+            <NotesListItem :selectedNote="selectedNote" :item="item" @note-selected="$emit('note-selected', item.id)" @switch-reminder="$emit('switch-reminder', item.id)" />
+        </div>  
+    </div>  
+</template>
+
+<script>
+import NotesListItem from './NotesListItem.vue'
+    export default {
+        name: 'NotesList',
+        components: {
+            NotesListItem
+        },
+        props: {
+            notesList: Array,
+            selectedNote: Number
+        },
+        emits: ['note-selected', 'switch-reminder']
+    }
+</script>
+
+<style lang="sass" scoped>
+::-webkit-scrollbar
+  width: 0
+
+.notes-list
+    display: flex
+    flex-flow: column
+    width: 10rem
+    height: 10rem
+    background-color: #000
+    color: magenta
+    overflow-y: scroll
+</style>
