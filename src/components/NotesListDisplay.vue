d0b53ffb9f8afa2ab1f097b2e7a08a88c3b584d8
Roman Pytalev <roman@pytalev.com>
2021-11-04 15:09:50 +0300

    Created NotesListDisplay component

---
+++ src/components/NotesListDisplay.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="notesListDisplay">
+        <DeleteNoteBtn :deleteNoteId="deleteNoteId" @delete-note="deleteNote" />
+        <NotesList :selectedNote="selectedNote" :notesList="notesList" @note-selected="noteSelected" @switch-reminder="switchReminder" />
+    </div>
+</template>
+
+<script>
+import DeleteNoteBtn from './DeleteNoteBtn.vue'
+import NotesList from './NotesList.vue'
+    export default {
+        name: 'NotesListDisplay',
+        components: {
+            DeleteNoteBtn,
+            NotesList
+        },
+        props: {
+            notesList: Array,
+            deleteNoteId: Number,
+            selectedNote: Number
+        },
+        methods: {
+            switchReminder(id) {
+                this.$emit('switch-reminder', id);
+            },
+            deleteNote() {
+                this.$emit('delete-note');
+            },
+            noteSelected(id) {
+                this.$emit('note-selected', id);
+            }
+        },
+        emits: ['switch-reminder', 'delete-note', 'note-selected']
+    }
+</script>
+
+<style lang="sass" scoped>
+.notesListDisplay
+    width: 20rem
+    height: 20rem
+    margin: 1rem
+    background-color: pink
+    color: magenta
+</style>
